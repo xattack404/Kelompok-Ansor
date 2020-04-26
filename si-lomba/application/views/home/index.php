8b608a2683d39f17a8c9b892d8160a0323ebe212
Lambang Arinanda Hadi <dr.naba.upptn@gmail.com>
2020-04-26 13:42:15 +0700

    masuk CI

---
+++ si-lomba/application/views/home/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en" id="home">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="<?= base_url('/'); ?>assets/style.css">
+    <link rel="stylesheet" href="<?= base_url('/'); ?>assets/fontawesome/css/all.min.css">
+    <!-- jquery -->
+    <script src="<?= base_url('/'); ?>assets/js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<!-- Jumbotron -->
+<div class="jumbotron">
+    <div class="jumbotron-content">
+        <h1>SELAMAT DATANG DI <br> SISTEM INFORMASI LOMBA</h1>
+        <p>asah kemampuanmu dengan mengikuti lomba dan jadilah juara</p>
+    </div>
+</div>
+<!-- Akhir Jumbotron -->
+
+<!-- Konten -->
+<div class="content" id="konten">
+    <div class="judul-konten">
+        <h1>last event</h1>
+        <p>semua lomba terbaru hari ini</p>
+    </div>
+    <div class="katalog">
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="lomba.php">
+                        <img src="assets/IMGLomba/sampel.jpg" alt="">
+                        <label for="">Piala walikota Bekasi</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel2.jpeg" alt="">
+                        <label for="">PIALA WALIKOTA SOLO 6 - ROAD RACE</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel5.jpg" alt="">
+                        <label for="">JATENG OPEN 2018 HOCKEY</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel3.jpeg" alt="">
+                        <label for="">Jakarta Open 2019</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel4.jpg" alt="">
+                        <label for="">Jakarta Open 2019</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel6.jpg" alt="">
+                        <label for="">Piala Menpora 2018</label>
+                    </a>
+                </div>
+            </div>
+    </div>
+    <div class="more">
+        <a href="">
+            More event <br> <i class="fas fa-ellipsis-h"></i>
+        </a>
+    </div>
+    
+</div>
+<!-- Akhir Konten -->
+
+<!-- Button Kontak -->
+<div class="kontak">
+    <a href="">Contact</a>
+</div>
+<!-- Button kontak akhir -->
+
+
+ <!-- top -->
+ <top id="top">
+    <a href="#home" class="page-scroll">
+        <i class="fas fa-arrow-up"></i>
+    </a>
+</top>
+    <script type="text/javascript">
+        window.addEventListener("scroll", function(){
+            var top = document.querySelector("top");
+            top.classList.toggle("totop", window.scrollY > 0);
+        })
+    </script>
+ <!-- top akhir -->
+
+
+<footer>
+    <div class="copyright">
+        <h4><a href="#">Sistem Informasi Lomba</a></h4>
+        <i class="fa fa-copyright" aria-hidden="true"> Template by: <a href="#">Kelompok 4</a></i>
+    </div>
+</footer>
+
+<script src="<?= base_url('/'); ?>assets/js/style.js"></script>
+</body>
+</html>
---
+++ si-lomba/application/views/home/lomba.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" id="home">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lomba</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+    <!-- jquery -->
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<!-- konten-->
+<form class="box-lomba">
+    <div class="info-lomba">
+        <h1>Lomba tidak Tersedia</h1>
+    </div>
+    <div class="img">
+        <img src="assets/IMGLomba/sampel.jpg" alt="">    
+    </div>
+    <div class="konten-lomba">
+        <h3>PIALA WALIKOTA BEKASI 2020</h3>
+        <p><label for="">KATEGORI :</label> Olahraga</p>
+        <p><label for="">UMUR :</label> 20-25</p>
+        <p><label for="">TINGKAT :</label> Umum</p>
+        <p><label for="">KETERANGAN : <br> <br></label>
+            Will be held at Lapangan Sepatu Roda Patriot Kota Bekasi (GOR)
+            Registration open until Wednesday, 11 March 2020
+            Organizer: Walikota Bekasi
+            Email: letsbekasiopen2019@gmail.com
+            Contact:
+            Edi: 081918606060
+            Danu: 081290193141
+            Participant: 297 Athlete. 31 Club.
+        </p>
+        <a href=""><button type="button">Panduan Lomba</button></a>
+        <a href="registrasi.php"><button type="button">Registrasi</button></a>
+    </div>
+</form>
+<!-- Akhir konten-->
+
+
+<footer>
+    <div class="copyright">
+        <h4><a href="#">Sistem Informasi Lomba</a></h4>
+        <i class="fa fa-copyright" aria-hidden="true"> Template by: <a href="#">Tahajjudin Fajri</a></i>
+    </div>
+</footer>
+
+<script src="assets/js/style.js"></script>
+</body>
+</html>
