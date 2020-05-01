2530861cd55db6d754159c79625b692515677652
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-05-01 17:34:58 +0700

    update

--- simba/resources/views/kategori/create.blade.php
+++ simba/resources/views/kategori/create.blade.php
@@ -2,20 +2,23 @@
 
 @section('content')
                 <h1>Tambah Kategori</h1>
-                    <form method="POST" action="{{ route('kategori.store') }}">
+                    <form class="box-form" method="POST" action="{{ route('kategori.store') }}">
                         @csrf
 
-                        <div class="">
+                        <div class="input-kategori">
                             <label for="name" class="col-md-4 col-form-label text-md-right">Nama Kategori</label>
                             <input id="nama_kategori" type="text" class="form-control @error('nama_kategori') is-invalid @enderror" name="nama_kategori" value="{{ old('nama_kategori') }}" required >
                         </div>
 
-                        <div class="">
+                        <div class="input-kategori">
                             <label for="harga" class="col-md-4 col-form-label text-md-right">Harga</label>
                             <input id="harga" type="number" class="form-control @error('harga') is-invalid @enderror" name="harga" value="{{ old('harga') }}" required >
                         </div>
-                                <button type="submit" class="btn btn-primary">
+                                <button class="btn-admin" type="submit" class="btn btn-primary">
                                     Simpan
+                                </button>
+                                <button class="btn-admin" type="submit" class="btn btn-primary">
+                                    kembali
                                 </button>
                     </form>
 @endsection
