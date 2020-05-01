c6c8a7789b1f460ee798c997cb83c2b9a65a3682
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-05-01 16:53:54 +0700

    halaman kategori

--- simba/resources/views/kategori/index.blade.php
+++ simba/resources/views/kategori/index.blade.php
@@ -1,30 +1,32 @@
 @extends('layouts.admin')  
 @section('content')
     <h1>admin pannel</h1>
-    <a href="{{ route('kategori.create') }}">Tambah Kategori Baru</a>
-      <table border="1" cellspacing="0" cellpadding="5" class="">
-        <thead>
-                <tr>
-                  <th scope="col">NO</th>
-                  <th scope="col">Nama Kategori</th>
-                  <th scope="col">Harga</th>
-                  <th scope="col">Action</th>
-                </tr>
-        </thead>
-        <tbody>
-               @forelse($data as $kategori)
-                <tr>
-                  <td>
+    <div class="box-kategori">
+      <a class="btn-admin" href="{{ route('kategori.create') }}">Tambah Kategori</a>
+        <table border="1" cellspacing="0" cellpadding="5" class="">
+          <thead>
+                  <tr>
+                    <th scope="col">NO</th>
+                    <th scope="col">Nama Kategori</th>
+                    <th scope="col">Harga</th>
+                    <th scope="col">Action</th>
+                  </tr>
+          </thead>
+          <tbody>
+                @forelse($data as $kategori)
+                  <tr>
+                    <td>
 
-                  </td>
-                  <td>{{ $kategori->nama_kategori }}</td>
-                  <td>{{ $kategori->harga }}</td>
-               @empty
-                  <td>
+                    </td>
+                    <td>{{ $kategori->nama_kategori }}</td>
+                    <td>{{ $kategori->harga }}</td>
+                @empty
+                    <td>
 
-                  </td>
-                </tr>
-                @endforelse       
-        </tbody>
-    </table>
+                    </td>
+                  </tr>
+                  @endforelse       
+          </tbody>
+      </table>
+    </div>
 @endsection
