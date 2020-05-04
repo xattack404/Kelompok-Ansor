f5983fd780341adc2553e7ac7c5a177dac9d6ac7
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-05-05 06:22:23 +0700

    update tahaj

--- simba/resources/views/cabangolahraga/create.blade.php
+++ simba/resources/views/cabangolahraga/create.blade.php
@@ -7,44 +7,74 @@
   </div>
 
   <div class="section-body">
-    <div class="col-12 col-md-6 col-lg-6">
-        <div class="card">
-          <div class="card-header">
-            <a href="{{ route('cabangolahraga.index') }}"> 
-              <button type="button" class="btn btn-outline-info">
-                <i class="fas fa-arrow-circle-left"></i> Back
-              </button>
-          </a>
-          </div>
-          <div class="card-body">
-            <form action="{{ route('cabangolahraga.store') }}" method="POST" enctype="multipart/form-data">
-              @csrf
-              <div class="form-group">
-                <label>Name Olahraga</label>
-                <input type="text" name="nama_or" class="form-control" required>
-                <div class="form-group"><label>Deskripsi Lomba</label>
-            <textarea class="form-control" rows="10" id="deskripsi" name="deskripsi"></textarea>
-                <select required name="kategori" class="form-control" data-live-search="true">
-                @foreach($data['kategori'] as $kategori)
-                <option value="{{ $kategori->id }}">{{ $kategori->nama_kategori }} </option>
-                @endforeach
-                </select>
-                <label>Tanggal Pelaksanaan</label>
-                <input type="date" name="tanggal_pelaksanaan" class="form-control" required>
-                <div class="form-group"><label>* Foto Baru</label><br />
-            <input type="file" name="foto" id="foto" onchange="tampilkanPreview(this,'preview')" required />
-            </div>  
-            <br><b>Preview Gambar</b><br>
-            <img id="preview" src="" alt="" width="35%" />
-                      </div>
-              <div class="form-group">
-                <button type="submit" class="btn btn-primary">SAVE</button>
-              </div>
-              </form>
-          </div>
-        </div>
-      </div>  
+    <!-- halaman1 -->
+      <div class="col-12 col-md-12 col-lg-12">
+          <div class="card">
+            <div class="card-header">
+              <a href="{{ route('cabangolahraga.index') }}"> 
+                <button type="button" class="btn btn-outline-info">
+                  <i class="fas fa-arrow-circle-left"></i> Back
+                </button>
+              </a>
+            </div>
+
+            <div class="card-body">
+              <form action="{{ route('cabangolahraga.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                  <div class="form-group">
+                    <label>Name Olahraga</label>
+                    <input type="text" name="nama_or" class="form-control" required>
+                  </div>
+                  
+                  <div class="form-group">
+                    <select required name="kategori" class="form-control" data-live-search="true">
+                    @foreach($data['kategori'] as $kategori)
+                    <option value="{{ $kategori->id }}">{{ $kategori->nama_kategori }} </option>
+                    @endforeach
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Tanggal Pelaksanaan</label>
+                    <input type="date" name="tanggal_pelaksanaan" class="form-control" required>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Deskripsi Lomba</label>
+                    <div class="">
+                      <div id="editor">
+                        <h1>TULIS KETERANGAN</h1>
+                      </div>    
+			              </div>
+                    <script>
+                      initSample();
+                    </script>
+                    <!-- <textarea class="form-control" rows="10" id="deskripsi" name="deskripsi" style="min-height: 300px"></textarea> -->
+                  </div>
+
+                  <div class="form-group">
+                    <label>* Foto Baru</label> <br>
+                    <input type="file" name="foto" id="foto" onchange="tampilkanPreview(this,'preview')" required />
+                  </div>  
+
+                  <div class="form-group">
+                    <b>Preview Gambar</><br>
+                    <img id="preview" src="" alt="" width="35%" />
+                  </div>
+                
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary">SAVE</button>
+                  </div>
+                </form>
+            </div>
+          </div>             
+      </div>
+      <!-- halaman 2 -->
+      <div class="col-12 col-md-6 col-lg-6"> 
+        
+      </div>
   </div>
+
   <script>
 function tampilkanPreview(foto,idpreview)
 { //membuat objek gambar
--- simba/resources/views/layouts/adminmain.blade.php
+++ simba/resources/views/layouts/adminmain.blade.php
@@ -8,10 +8,14 @@
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <script src="{{ asset('assets/ckeditor/ckeditor.js') }}"></script>
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+  <script src="{{ asset('assets/ckeditor/samples/js/sample.js') }}"></script>
+  <link rel="stylesheet" href="{{ asset('assets/ckeditor/samples/css/samples.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/ckeditor/samples/toolbarconfigurator/lib/codemirror/neo.css') }}">
 </head>
 
 <body>
