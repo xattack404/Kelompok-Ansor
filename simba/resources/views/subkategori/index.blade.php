662a9050d7d108487410cd37e93a92c0170bd9fa
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-05-05 22:26:15 +0700

    Update index.blade.php

--- simba/resources/views/subkategori/index.blade.php
+++ simba/resources/views/subkategori/index.blade.php
@@ -27,7 +27,7 @@
               <button type="button" class="btn btn-primary">Tambah Data</button>
             </a>
           </div>
-          <div class="card-body">
+          <div class="card-body" style="overflow: scroll">
             <table class="table table-bordered">
               <thead>
                 <tr>
