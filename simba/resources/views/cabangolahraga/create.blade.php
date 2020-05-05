d32b12a5fdc3a69e2328acf4748db3f4f397ca48
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-05-05 09:38:21 +0700

    Update create.blade.php

--- simba/resources/views/cabangolahraga/create.blade.php
+++ simba/resources/views/cabangolahraga/create.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.adminmain')
 @section('content')
-<section class="section">
+<section class="section" >
   
   <div class="section-header">
     <h1>Cabang Olahraga <small>Tambah Data</small></h1>
   </div>
 
-  <div class="section-body">
+  <form class="section-body container" action="{{ route('cabangolahraga.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <div class="row" style=" display:grid; grid-template-columns: repeat(auto-fit,minmax(300px, 1fr)); grid-gap: 15px;">
     <!-- halaman1 -->
-      <div class="col-12 col-md-12 col-lg-12">
+      <div class="" style="width: ">
           <div class="card">
             <div class="card-header">
               <a href="{{ route('cabangolahraga.index') }}"> 
@@ -19,8 +21,6 @@
             </div>
 
             <div class="card-body">
-              <form action="{{ route('cabangolahraga.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
                   <div class="form-group">
                     <label>Name Olahraga</label>
                     <input type="text" name="nama_or" class="form-control" required>
@@ -40,19 +40,6 @@
                   </div>
 
                   <div class="form-group">
-                    <label>Deskripsi Lomba</label>
-                    <div class="">
-                      <div id="editor">
-                        <h1>TULIS KETERANGAN</h1>
-                      </div>    
-			              </div>
-                    <script>
-                      initSample();
-                    </script>
-                    <!-- <textarea class="form-control" rows="10" id="deskripsi" name="deskripsi" style="min-height: 300px"></textarea> -->
-                  </div>
-
-                  <div class="form-group">
                     <label>* Foto Baru</label> <br>
                     <input type="file" name="foto" id="foto" onchange="tampilkanPreview(this,'preview')" required />
                   </div>  
@@ -60,20 +47,36 @@
                   <div class="form-group">
                     <b>Preview Gambar</><br>
                     <img id="preview" src="" alt="" width="35%" />
-                  </div>
-                
-                  <div class="form-group">
-                    <button type="submit" class="btn btn-primary">SAVE</button>
-                  </div>
-                </form>
+                  </div> 
             </div>
           </div>             
       </div>
       <!-- halaman 2 -->
-      <div class="col-12 col-md-6 col-lg-6"> 
-        
+      <div class="" style="width: "> 
+          <div class="card">
+            <div class="card-body">
+
+              <div class="form-group">
+                <label>Deskripsi Lomba</label>
+                  <div class="">
+                      <div id="editor" style="min-height: 700px">
+                        <h1>TULIS KETERANGAN</h1>
+                      </div>    
+			            </div>
+                  <script>
+                      initSample();
+                  </script>
+                  <!-- <textarea class="form-control" rows="10" id="deskripsi" name="deskripsi" style="min-height: 300px"></textarea> -->
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">SAVE</button>
+                </div>
+            </div>
+          </div>
       </div>
-  </div>
+    </div>
+</form>
 
   <script>
 function tampilkanPreview(foto,idpreview)
