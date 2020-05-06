b5007a66fc2fb6ab79560bcf54f9d75bb9bab4b5
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-05-06 20:25:29 +0700

    update halaman user sementara

---
+++ backup projek sememster 4/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en" id="home">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+    <!-- jquery -->
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<!-- Jumbotron -->
+<div class="jumbotron">
+    <div class="jumbotron-content">
+        <h1>SELAMAT DATANG DI <br> SISTEM INFORMASI LOMBA</h1>
+        <p>asah kemampuanmu dengan mengikuti lomba dan jadilah juara</p>
+    </div>
+</div>
+<!-- Akhir Jumbotron -->
+
+<!-- Konten -->
+<div class="content" id="konten">
+    <div class="judul-konten">
+        <h1>last event</h1>
+        <p>semua lomba terbaru hari ini</p>
+    </div>
+    <div class="katalog">
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="lomba.php">
+                        <img src="assets/IMGLomba/sampel.jpg" alt="">
+                        <label for="">Piala walikota Bekasi</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel2.jpeg" alt="">
+                        <label for="">PIALA WALIKOTA SOLO 6 - ROAD RACE</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel5.jpg" alt="">
+                        <label for="">JATENG OPEN 2018 HOCKEY</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel3.jpeg" alt="">
+                        <label for="">Jakarta Open 2019</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel4.jpg" alt="">
+                        <label for="">Jakarta Open 2019</label>
+                    </a>
+                </div>
+            </div>
+            <div class="item-katalog">
+                <div class="item">
+                    <a href="">
+                        <img src="assets/IMGLomba/sampel6.jpg" alt="">
+                        <label for="">Piala Menpora 2018</label>
+                    </a>
+                </div>
+            </div>
+    </div>
+    <div class="more">
+        <a href="semualomba.php">
+            More event <br> <i class="fas fa-ellipsis-h"></i>
+        </a>
+    </div>
+    
+</div>
+<!-- Akhir Konten -->
+
+<!-- Button Kontak -->
+<div class="kontak">
+    <a href="">Contact</a>
+</div>
+<!-- Button kontak akhir -->
+
+
+ <!-- top -->
+ <top id="top">
+    <a href="#home" class="page-scroll">
+        <i class="fas fa-arrow-up"></i>
+    </a>
+</top>
+    <script type="text/javascript">
+        window.addEventListener("scroll", function(){
+            var top = document.querySelector("top");
+            top.classList.toggle("totop", window.scrollY > 0);
+        })
+    </script>
+ <!-- top akhir -->
+
+ <?php include 'footer.php'; ?>
+
+
+<script src="assets/js/style.js"></script>
+</body>
+</html>
---
+++ backup projek sememster 4/navbar.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" id="home">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+    <!-- jquery -->
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+<!-- navbar -->
+<nav id="nav">
+    <!-- logo-->
+    <div class="logo"><a href="#">LOGO</a> </div>
+    <!-- menu -->
+    <div class="menu" id="menu">
+        <ul>        
+            <div class="close"><a href="#nav" title="close"><i class="fas fa-times white"></i></a> </div>
+                <li><a class="page-scroll" href="index.php">Home</a></li>
+                <li><a class="page-scroll" href="#konten">Last Event</a></li>
+                <li>
+                    <h5>
+                        Kategori
+                        <i class="fas fa-caret-down"></i>
+                        <i class="fas fa-sort-up"></i>
+                    </h5>
+                    <ul class="dropdown" id="dropdown">
+                        <li><a href="">Fotografi</a></li>
+                        <li><a href="">Logo</a></li>
+                        <li><a href="">Video</a></li>
+                        <li><a href="">Olimpiade</a></li>
+                        <li><a href="">Olahraga</a></li>
+                    </ul>
+                </li>
+                <li><a class="page-scroll" href="registrasi.php">Registrasi</a></li>
+        </ul>               
+    </div>
+    <div class="menu-toggle">
+        <a href="#menu" title="menu"><i class="fas fa-bars white" aria-hidden="true"></i></a>
+    </div>
+    <!-- search -->
+    <div class="search-form" id="search-form">
+        <form action="">
+            <input id="" type="search" placeholder="search and enter...">
+            <button type="submit" title="cari"><i class="fas fa-search"></i></button>
+        </form>
+    </div>
+    <div class="search-icon">
+        <a href="#search-form" title="cari"><i class="fa fa-search white"></i></a>
+        <a href="#nav" title="close"><i class="fas fa-times white"></i></a>
+    </div>
+</nav>
+<!-- akhir navbar -->
+
+<script src="assets/js/style.js"></script>
+</body>
+</html>
