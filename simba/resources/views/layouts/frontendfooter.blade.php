32ad309e5352f5bcbfbc84210c04dedcea1c26c3
tahajjudinfajri <55199055+tahajjudin@users.noreply.github.com>
2020-05-09 23:46:50 +0700

    Revert "update"
    This reverts commit cc24dd5f371ab44c2e703c1017044bdcfdc85949.

--- simba/resources/views/layouts/frontendfooter.blade.php
+++ simba/resources/views/layouts/frontendfooter.blade.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" id="home">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="{{ asset('assets_frontend/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets_frontend/fontawesome/css/all.min.css') }}">
+    <!-- jquery -->
+    <script src="{{ asset('assets_frontend/js/jquery-3.4.1.min.js') }}"></script>
+</head>
+<body>
 <footer>
     <div class="copyright">
         <h4><a href="#">Sistem Informasi Lomba</a></h4>
         <i class="fa fa-copyright" aria-hidden="true"> Template by: <a href="#">Kelompok 4</a></i>
     </div>
 </footer>
+</body>
+</html>
