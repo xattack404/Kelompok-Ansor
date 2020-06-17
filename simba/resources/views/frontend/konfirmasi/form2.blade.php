3452570d76d3d97b84f7448fdd0113ef021886c7
E41181803-Lambang Arinanda Hadi <55471760+xattack404@users.noreply.github.com>
2020-06-17 14:07:29 +0700

    aa

--- simba/resources/views/frontend/konfirmasi/form2.blade.php
+++ simba/resources/views/frontend/konfirmasi/form2.blade.php
@@ -1,6 +1,6 @@
 <form action="" class="popup-tambah" id="tambah">
     @csrf
-    <div class="no-atlit">Nomor Lomba Atlit Aktif (ID: <span>{{ $data['pembayaran']->detailPembayaran[0]->atletAktif->atlet->id}})</span></div>
+    <div class="no-atlit">Nomor Lomba Atlit Aktif (ID: <span>{{ $data['pembayaran']->detailPembayaran[0]->atletAktif->atlet->nik_id}})</span></div>
     <div class="aksi-popup">
         <button type="submit" style="background-color: #90e98b;">Tambah Lomba</button>
         <button type="submit" style="background-color: #2980b9;">Simpan Perubahan</button>
@@ -17,13 +17,15 @@
                 <th style="min-width: 100px;">Kategori</th>
                 <th style="min-width: 100px;">Pilih Lomba</th>
             </tr>
-            @foreach($data['pembayaran']->detailPembayaran[0]->atletAktif->atlet as $atlet)
+            @php
+            $detail =$data['pembayaran']->detailPembayaran[0];
+            @endphp
             <tr>
-                <td>{{ $atlet->nama }}</td>
-                <td>{{ $atlet->tgl_lahir }}</td>
-                <td>{{ $atlet->jenis_kelamin }}</td>
+                <td>{{ $detail->atletAktif->atlet->nama }}</td>
+                <td>{{ $detail->atletAktif->atlet->tgl_lahir }}</td>
+                <td>{{ $detail->atletAktif->atlet->jenis_kelamin }}</td>
                 <td>kelas </td>
-                <td>$data['pembayaran']->detailPembayaran->kategoriRelasi->nama_kategori</td>
+                <td>{{ $detail->kategoriRelasi->nama_kategori }}</td>
                 <td>
                     <select name="lomba" id="lomba">
                         <option value="">--pilih--</option>
