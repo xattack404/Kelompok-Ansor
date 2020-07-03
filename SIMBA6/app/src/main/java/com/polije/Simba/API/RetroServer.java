16ef095f62950de42c67dfcecb933f5d6d9b2aac
muhammadfarhannurpratama <55246227+muhammadfarhannurpratama@users.noreply.github.com>
2020-07-03 18:54:51 +0700

    update

--- SIMBA6/app/src/main/java/com/polije/Simba/API/RetroServer.java
+++ SIMBA6/app/src/main/java/com/polije/Simba/API/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String baseURL = "http://192.168.1.7/api/";
+    private static final String baseURL ="http://192.168.1.3/api/";
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
